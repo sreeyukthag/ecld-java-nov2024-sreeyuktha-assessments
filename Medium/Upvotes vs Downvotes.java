1a6dedcf01c0ec5de35c80a80fb49f93280c5870
sreeyukthag <sreeyuktha.g@edstem.com>
2024-11-27 08:58:32 +0530

    Create Upvotes vs Downvotes.java

---
+++ Medium/Upvotes vs Downvotes.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main {
+        public static int getVoteCount(int upVotes,int downVotes) {
+            if (upVotes>downVotes)
+                return upVotes-downVotes;
+            else
+                return -(downVotes-upVotes);
+
+        }
+
+    public static void main(String[] args) {
+            Scanner s=new Scanner(System.in);
+            System.out.println("Enter Upvotes : ");
+            int upVotes=s.nextInt();
+
+            System.out.println("Enter Downvotes : ");
+            int downVotes=s.nextInt();
+
+        System.out.println("Total vote : "+getVoteCount(upVotes,downVotes));
+    }
+
+}
